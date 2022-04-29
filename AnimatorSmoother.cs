5a927ed61ab27565051c58cfeaa00292e691b62c
Naelstrof <naelstrof@gmail.com>
2022-04-29 15:30:54 -0600

    documentation

---
+++ AnimatorSmoother.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor.Animations;
+using UnityEditor;
+
+public class AnimatorSmoother {
+    /*[MenuItem("Tools/Naelstrof/ReadAnimation")]
+    public static void ReadAnimation() {
+        AnimationClip[] animations = Selection.GetFiltered<AnimationClip>(SelectionMode.Assets);
+        if (animations.Length != 1) {
+            throw new UnityException("Failed: Select a SINGLE animator controller to generate them on!!!!");
+        }
+        AnimationClip clip = animations[0];
+        foreach(var binding in AnimationUtility.GetCurveBindings(clip)) {
+            Debug.Log("Path: " + binding.path + "\n" + "Prop: "+binding.propertyName);
+        }
+    }*/
+    [MenuItem("Tools/Naelstrof/Generate AnimatorSmoothers")]
+    public static void GenerateAnimatorSmoothers() {
+        AnimatorController[] animators = Selection.GetFiltered<AnimatorController>(SelectionMode.Assets);
+        if (animators.Length != 1) {
+            throw new UnityException("Failed: Select a SINGLE animator controller to generate them on!!!!");
+        }
+        AnimatorController controller = animators[0];
+        foreach(AnimatorControllerParameter parameter in controller.parameters) {
+            if (parameter.name.Contains("Smooth")) {
+                continue;
+            }
+            if (parameter.type != AnimatorControllerParameterType.Float) {
+                continue;
+            }
+            if (ContainsSmoothedVersion(parameter,controller)) {
+                continue;
+            }
+            GenerateSmoothed(parameter, controller);
+        }
+        Debug.Log("Done! Check Assets/ for new animations.");
+    }
+    static bool ContainsSmoothedVersion(AnimatorControllerParameter sourceParameter, AnimatorController controller) {
+        foreach(AnimatorControllerParameter parameter in controller.parameters) {
+            if (parameter.name.Contains(sourceParameter.name) && parameter.name.Contains("Smooth")) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static void GenerateSmoothed(AnimatorControllerParameter sourceParameter, AnimatorController controller) {
+        string smoothedName = sourceParameter.name+"Smoothed";
+        string smoothAmountName = sourceParameter.name+"SmoothAmount";
+        controller.AddLayer(sourceParameter.name + "Smoother");
+        controller.AddParameter(smoothedName, AnimatorControllerParameterType.Float);
+        controller.parameters[controller.parameters.Length-1].defaultFloat = sourceParameter.defaultFloat;
+        controller.AddParameter(smoothAmountName, AnimatorControllerParameterType.Float);
+        controller.parameters[controller.parameters.Length-1].defaultFloat = 0.1f;
+        AnimatorControllerLayer newLayer = controller.layers[controller.layers.Length-1];
+        newLayer.defaultWeight = 1f;
+        //AnimatorState state = newLayer.stateMachine.AddState(parameter.name+"Smoother");
+
+        AnimationClip zero = GenerateAnimation(smoothedName, 0f);
+        AnimationClip one = GenerateAnimation(smoothedName, 1f);
+
+        BlendTree firstBlend;
+        controller.CreateBlendTreeInController(sourceParameter.name+"Smoother", out firstBlend, controller.layers.Length-1);
+        firstBlend.blendType = BlendTreeType.Simple1D;
+        firstBlend.blendParameter = smoothAmountName;
+        firstBlend.hideFlags = HideFlags.HideInHierarchy;
+
+        BlendTree currentValue;
+        controller.CreateBlendTreeInController("CurrentValue", out currentValue, controller.layers.Length-1);
+        currentValue.blendType = BlendTreeType.Simple1D;
+        currentValue.blendParameter = smoothedName;
+        currentValue.AddChild(zero, 0f);
+        currentValue.AddChild(one, 1f);
+        currentValue.hideFlags = HideFlags.HideInHierarchy;
+
+        BlendTree wantedValue;
+        controller.CreateBlendTreeInController("WantedValue", out wantedValue, controller.layers.Length-1);
+        wantedValue.blendType = BlendTreeType.Simple1D;
+        wantedValue.blendParameter = sourceParameter.name;
+        wantedValue.AddChild(zero, 0f);
+        wantedValue.AddChild(one, 1f);
+        wantedValue.hideFlags = HideFlags.HideInHierarchy;
+
+        firstBlend.AddChild(currentValue, 0f);
+        firstBlend.AddChild(wantedValue, 1f);
+
+        AnimatorState firstState = newLayer.stateMachine.AddState(sourceParameter.name+"Smoother");
+        firstState.motion = firstBlend;
+
+        newLayer.stateMachine.defaultState = firstState;
+
+        EditorUtility.SetDirty(controller);
+    }
+
+    static AnimationClip GenerateAnimation(string parameter, float amount) {
+        AnimationClip clip = new AnimationClip();
+        clip.name = parameter+"_"+amount.ToString("F0");
+        string dir = "Assets/"+clip.name+".anim";
+        AnimationUtility.SetEditorCurve(clip, EditorCurveBinding.FloatCurve("",typeof(Animator), parameter), AnimationCurve.Constant(0f,1f,amount));
+        AssetDatabase.CreateAsset(clip,dir);
+        return AssetDatabase.LoadAssetAtPath<AnimationClip>(dir);
+    }
+}
+#endif
---
+++ README.md
@@ -0,0 +1,129 @@
+# VRCLipSyncOSC
+
+A lightweight C++ implementation of HTC Lipsync -> VRChat OSC. Configured with lua!
+
+Its intention is to allow users to convert raw HTC lipsync variables into usable floats within VRChat for facetracking and the like.
+
+## Features
+
+* Lua powered! Compress and massage the data into usable avatar parameters using simple lua configuration.
+* Share the `config.lua` with your friends! Get exactly the configuration that works for you and your community.
+* Doesn't busy-wait! HTC's very own unity package spawns a worker thread that entirely occupies a core waiting for new data... :pensive: This app doesn't do that.
+* Really tiny! Entire package is under a megabyte.
+
+## Installation
+
+1. Download and install the SRanipal Runtime found here (account required): https://developer-express.vive.com/resources/vive-sense/eye-and-facial-tracking-sdk/download/latest/
+2. Then simply run the executable found within [Releases](https://github.com/naelstrof/VRCLipSyncOSC/releases).
+
+## Usage (Avatar Setup)
+
+Warning: This section assumes you have decent tech-artists skills, and that you're using the default `config.lua` included with the package.
+
+1. Set up the following Avatar Parameters as floats:
+
+```
+JawOpen
+Smile
+Teeth
+MouthX
+Tongue
+TongueX
+TongueY
+```
+![Avatar parameter image](AvatarParameters.png)
+
+2. Set up the following blends on your avatar:
+
+Teeth/Lips blend:
+
+![Lips blend](LipsBlend.webm)
+
+Smile blend:
+
+![Smile blend](SmileBlend.webm)
+
+Tongue blend
+
+![Tongue blend](TongueBlend.webm)
+
+Jaw blend
+
+![Jaw blend](JawBlend.webm)
+
+Pout blend
+
+![Pout blend](PoutBlend.webm)
+
+
+With the blends set up, now we need to tackle one last problem. OSC parameters only update about 4 times a second for other players, to prevent it from being choppy we can use [hai's implementation](https://hai-vr.notion.site/Avatars-3-0-Animated-Animator-Parameters-and-Smoothing-f128c71dd3184c2bb61a4cff8296ada5#aeb2d0d54edf41e1a846818657dfc1b7) of Animator smoothing to have it appear smooth for other players.
+
+3. Copy AnimatorSmoother.cs (included in the package) into your Unity project somewhere.
+
+4. Select your Animator in the Project tab, then hit Tools->Naelstrof->Generate Animator Smoothers.
+
+![Animator smoother tool](AnimatorSmootherTool.png)
+
+This should generate a bunch of new parameters and layers designed to smooth the original values, it's very dumb and will generate them for ALL float parameters though.
+
+5. Delete unecessary smoothing layers (GestureLeftWeightSmoother and the like). We only want to smooth the face-tracking OSC parameters.
+
+6. Ensure that the smoothing layers have a weight of 1.
+
+![AnimatorSmoothLayer Weight](AnimatorSmoothLayerWeight.png)
+
+7. Customize the SmoothAmount of each parameter, it represents how slowly it moves toward the desired value. A value of 0.25 is a pretty good starting point.
+
+![AnimatorSmoothAmount Values](AnimatorSmoothAmountValues.png)
+
+8. Finally, use the `Smoothed` version of the variables within all of your blends.
+
+9. You're done! Test your changes in VR with the VRCLipSyncOSC app running.
+
+## Usage (VRCLipSyncOSC configuration)
+
+1. All configuration happens within config.lua. lipdata is a table with the following keys:
+```
+Jaw_Right
+Jaw_Left
+Jaw_Forward
+Jaw_Open
+Mouth_Ape_Shape
+Mouth_Upper_Right
+Mouth_Upper_Left
+Mouth_Lower_Right
+Mouth_Lower_Left
+Mouth_Upper_Overturn
+Mouth_Lower_Overturn
+Mouth_Pout
+Mouth_Smile_Right
+Mouth_Smile_Left
+Mouth_Sad_Right
+Mouth_Sad_Left
+Cheek_Puff_Right
+Cheek_Puff_Left
+Cheek_Suck
+Mouth_Upper_UpRight
+Mouth_Upper_UpLeft
+Mouth_Lower_DownRight
+Mouth_Lower_DownLeft
+Mouth_Upper_Inside
+Mouth_Lower_Inside
+Mouth_Lower_Overlay
+Tongue_LongStep1
+Tongue_LongStep2
+Tongue_Down
+Tongue_Up
+Tongue_Right
+Tongue_Left
+Tongue_Roll
+Tongue_UpLeft_Morph
+Tongue_UpRight_Morph
+Tongue_DownLeft_Morph
+Tongue_DownRight_Morph
+```
+You can find out what the keys represent by checking out HTC Vive's official documentation here: https://developer-express.vive.com/resources/vive-sense/eye-and-facial-tracking-sdk/documentation/
+
+2. config.lua must have an update function that takes the lipdata table, this is ran every update in order to send data to VRChat.
+3. Make sure to call `SendData("/avatar/parameters/myparameter", value)` before the end of `update` in order to send the parameters to VRChat. The values are cached and sent at the end.
+4. If there's an error in `config.lua`, the executable will report the error and immediately close. A convenience bat file has been included so that the error message can be read. Use it if the executable flashes or blinks when you run it.
